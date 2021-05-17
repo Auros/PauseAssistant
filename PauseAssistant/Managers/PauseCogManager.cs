8468afc14a2757e6cd715a249cd83ee04423a2c6
Auros Nexus <aurosnex@gmail.com>
2021-05-16 21:54:29 -0400

    Initial commit

---
+++ PauseAssistant/Managers/PauseCogManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zenject;
+
+namespace PauseAssistant.Managers
+{
+    internal class PauseCogManager : IInitializable
+    {
+        private readonly PauseMenuManager _pauseMenuManager;
+
+        public PauseCogManager(PauseMenuManager pauseMenuManager)
+        {
+            _pauseMenuManager = pauseMenuManager;
+        }
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
